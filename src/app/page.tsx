1001272d964017e2e054b3b1e91725bc65a20e07
leoziondev <leozionsuporte@gmail.com>
2023-11-26 20:50:18 -0300

    redirect to dashboard counter page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
-import Image from 'next/image'
+import { redirect } from 'next/navigation'
 
 export default function Home() {
-  return (
-    <>
-    <h1>Hola mundo!</h1></>
-  )
+  redirect('/dashboard/counter');
 }
