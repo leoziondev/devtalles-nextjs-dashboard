0748b169b669f83c4ba5ebc9194eadbf84ab1aff
leoziondev <leozionsuporte@gmail.com>
2023-11-27 15:55:52 -0300

    data fetching

--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -1,5 +1,17 @@
-export default function PokemonsPage() {
+const getPokemons = async ( limit = 20, offset = 0 ) => {
+  const data = await 
+    fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`)
+      .then((res) => res.json());
+
+    return data;
+}
+ 
+export default async function PokemonsPage() {
+  const pokemons = await getPokemons();
+  
   return (
-    <div>page</div>
+    <div>
+      {JSON.stringify(pokemons)}
+    </div>
   )
 }
