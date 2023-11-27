f48a77363ee6443a24c7153869b85213072d4e92
leoziondev <leozionsuporte@gmail.com>
2023-11-27 12:38:53 -0300

    use client component counter

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -1,8 +1,15 @@
+import { CartCounter } from "@/shopping-cart";
+
+export const metadata = {
+ title: 'Cart Products',
+ description: 'Count product cart',
+};
 
 export default function CounterPage() {
   return (
-    <div>
-      <h1>Counter Page</h1>
+    <div className="flex flex-col items-center justify-center w-full h-full">
+      <span>Cart Products</span>
+      <CartCounter value={12} />
     </div>
   );
 }
