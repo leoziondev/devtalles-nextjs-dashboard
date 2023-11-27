d638346bc7f4471c44e64d42f8ec97f418c5d753
leoziondev <leozionsuporte@gmail.com>
2023-11-27 20:08:19 -0300

    dynamic routes - get params

--- src/app/dashboard/pokemon/[id]/page.tsx
+++ src/app/dashboard/pokemon/[id]/page.tsx
@@ -1,8 +1,12 @@
 
-export default function PokemonPage() {
+interface Props {
+    params: { id: string};
+}
+
+export default function PokemonPage({ params }: Props) {
   return (
     <div>
-      <h1>Pokemon Page</h1>
+      <h1>Pokemon {params.id}</h1>
     </div>
   );
 }
