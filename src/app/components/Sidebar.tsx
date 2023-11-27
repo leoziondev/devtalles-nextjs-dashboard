f028a8aee9831bb385afa9b744032bc942715be8
leoziondev <leozionsuporte@gmail.com>
2023-11-27 16:15:32 -0300

    fix height screen

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -25,7 +25,7 @@ const menuItems = [
 
 export const Sidebar = () => {
   return (
-    <div id="menu" className="bg-gray-900 min-h-screen z-10 text-slate-300 w-96 left-0 h-screen overflow-y-scroll">
+    <div id="menu" className="bg-gray-900 min-h-screen z-10 text-slate-300 w-96 left-0 overflow-y-scroll">
           <div id="logo" className="my-4 px-6">
             <h1 className="flex items-center text-lg md:text-2xl font-bold text-white">
                 <IoLogoReact className="mr-1" />
