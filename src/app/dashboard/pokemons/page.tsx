3035ec8e68f05fa9a904e450278ede8dea3a5301
leoziondev <leozionsuporte@gmail.com>
2023-11-27 16:17:40 -0300

    assign data type and display images

--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -1,17 +1,35 @@
-const getPokemons = async ( limit = 20, offset = 0 ) => {
-  const data = await 
+import { PokemonsResponse, SimplePokemon } from "@/app/pokemons";
+import Image from "next/image";
+
+const getPokemons = async ( limit = 151, offset = 0 ): Promise<SimplePokemon[]> => {
+  const data: PokemonsResponse = await 
     fetch(`https://pokeapi.co/api/v2/pokemon?limit=${limit}&offset=${offset}`)
       .then((res) => res.json());
 
-    return data;
+    const pokemons = data.results.map((pokemon) => ({
+      id: pokemon.url.split('/').at(-2)!,
+      name: pokemon.name
+    }))
+
+    return pokemons;
 }
  
 export default async function PokemonsPage() {
   const pokemons = await getPokemons();
-  
+
   return (
     <div>
-      {JSON.stringify(pokemons)}
+      <div className="flex flex-wrap gap-10 items-center justify-center">
+        {pokemons.map(pokemon => (
+          <Image
+            key={pokemon.id}
+            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`}
+            width={100}
+            height={100}
+            alt={pokemon.name}
+          />
+        ))}
+      </div>
     </div>
   )
 }
