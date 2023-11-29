1962a9e8911e6c1e663b1b190f6dccc122004e82
leoziondev <leozionsuporte@gmail.com>
2023-11-29 12:10:51 -0300

    add pokemon type

--- src/pokemons/index.ts
+++ src/pokemons/index.ts
@@ -1,2 +1,3 @@
 export type { PokemonsResponse } from './interfaces/pokemons-response';
-export type { SimplePokemon } from './interfaces/single-pokemon'
+export type { SimplePokemon } from './interfaces/single-pokemon';
+export type { Pokemon } from './interfaces/pokemon';
