4a359d8e305767cf628e0b7872fcd88c613e485f
leoziondev <leozionsuporte@gmail.com>
2023-11-29 12:11:18 -0300

    get single pokemon data

--- src/app/dashboard/pokemon/[id]/page.tsx
+++ src/app/dashboard/pokemon/[id]/page.tsx
@@ -1,12 +1,27 @@
+import { Pokemon } from "@/pokemons";
 
 interface Props {
     params: { id: string};
 }
 
-export default function PokemonPage({ params }: Props) {
+const getPokemon = async(id: string): Promise<Pokemon> => {
+    const pokemon = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`,
+        {cache: 'force-cache'}
+    ).then((res) => res.json());
+
+    console.log(pokemon)
+
+    return pokemon;
+}
+
+export default async function PokemonPage({ params }: Props) {
+    const pokemon = await getPokemon(params.id)
   return (
     <div>
       <h1>Pokemon {params.id}</h1>
+      <div>
+        {JSON.stringify(pokemon)}
+      </div>
     </div>
   );
 }
