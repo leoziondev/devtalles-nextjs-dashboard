54ba233398efacfcf9c954f26b5fbee82458c254
leoziondev <leozionsuporte@gmail.com>
2023-11-30 11:34:37 -0300

    use widget in main page

--- src/app/dashboard/main/page.tsx
+++ src/app/dashboard/main/page.tsx
@@ -1,8 +1,14 @@
+import { SimpleWidget } from "@/components";
 
 export default function MainPage() {
   return (
-    <div>
-      <h1>Main Page</h1>
+    <div className="text-slate-800 p-2">
+      <h1 className="mt-2 text-3xl">Dashboard</h1>
+      <span className="text-md text-blue-500">General Information</span>
+
+      <div className="flex flex-wrap p-2 mt-8">
+        <SimpleWidget />
+      </div>
     </div>
   );
 }
