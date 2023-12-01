05492fe5b376e7ba812140e2cacc33e9532a359c
leoziondev <leozionsuporte@gmail.com>
2023-12-01 11:34:37 -0300

    get count value api

--- src/shopping-cart/components/CartCounter.tsx
+++ src/shopping-cart/components/CartCounter.tsx
@@ -9,13 +9,29 @@ interface Props {
     value?: number; 
 }
 
+export interface CounterResponse {
+    method: string
+    count: number
+}
+
+const getApiCounter = async(): Promise<CounterResponse> => {
+    const data = await fetch('/api/counter').then(res => res.json())
+
+    return data
+}
+
 export const CartCounter = ({ value = 0 }: Props) => {
     const count = useAppSelector(state => state.counter.count)
     const dispatch = useDispatch()
 
+    // useEffect(() => {
+    //     dispatch(initCounterState(value))
+    // }, [dispatch, value])
+
     useEffect(() => {
-        dispatch(initCounterState(value))
-    }, [dispatch, value])
+        getApiCounter()
+            .then(({ count }) => dispatch(initCounterState(count)))
+    }, [dispatch])
 
     return (
         <>
