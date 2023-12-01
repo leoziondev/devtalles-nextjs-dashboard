8907c1811d91ea272028e729bd43ab6b7a45a8a2
leoziondev <leozionsuporte@gmail.com>
2023-12-01 09:49:11 -0300

    counter slice

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,8 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { TypedUseSelectorHook, useDispatch, useSelector } from 'react-redux'
+
+import counterReducer from './counter/counterSlice'
 
 export const store = configureStore({
   reducer: {
-    
+    counterReducer,
   },
 })
 
@@ -10,3 +13,6 @@ export const store = configureStore({
 export type RootState = ReturnType<typeof store.getState>
 // Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch
+// Use throughout your app instead of plain `useDispatch` and `useSelector`
+export const useAppDispatch: () => AppDispatch = useDispatch
+export const useAppSelector: TypedUseSelectorHook<RootState> = useSelector
