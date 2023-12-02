de582f7658577cd43ac7a24430c3210b4d5d89ea
leoziondev <leozionsuporte@gmail.com>
2023-12-02 09:51:59 -0300

    add state favorites pokemons in localstorage

--- src/store/pokemons/pokemons.ts
+++ src/store/pokemons/pokemons.ts
@@ -5,7 +5,14 @@ interface PokemonsState {
     [key: string]: SimplePokemon
 }
 
-const initialState = {
+const getInitialState = (): PokemonsState => {
+    const favorites = JSON.parse(localStorage.getItem('favorite-pokemons') ?? '{}')
+
+    return favorites
+}
+
+const initialState: PokemonsState = {
+    ...getInitialState()
     // '1': { id: 1, name: 'bulbasur' },
     // '3': { id: 3, name: 'sassa' },
 }
@@ -21,10 +28,12 @@ const pokemonsSlice = createSlice({
         if (!!state[id]) {
             delete state[id]
 
-            return
+            //return
+        } else {
+            state[id] = pokemon
         }
 
-        state[id] = pokemon
+        localStorage.setItem('favorite-pokemons', JSON.stringify(state))
     }
   }
 });
